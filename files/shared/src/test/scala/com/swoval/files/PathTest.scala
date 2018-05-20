901a2623897355ad819f5d165e7d5b0f7cca697c
Ethan Atkins <ethan.atkins@gmail.com>
2018-05-19 19:46:24 -0700

    tmpdir

--- files/shared/src/test/scala/com/swoval/files/PathTest.scala
+++ files/shared/src/test/scala/com/swoval/files/PathTest.scala
@@ -1,8 +1,11 @@
 package com.swoval.files
 
+import java.nio.file.Paths
+
 import utest._
+
 import scala.util.Random
-import Path.{ separator => sep }
+import Path.{separator => sep}
 import com.swoval.test._
 
 object PathTest extends TestSuite {
@@ -73,7 +76,7 @@ object PathTest extends TestSuite {
     }
     'mkdir - {
       'absolute - {
-        val path = Path("", "tmp", s"foo${random.nextInt}")
+        val path = Paths.get(System.getProperty("java.io.tmpdir"), "").resolve(s"foo${random.nextInt}")
         path.delete()
         path.mkdir ==> path
         assert(path.exists)
